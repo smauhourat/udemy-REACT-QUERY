16762dafe5090d3c2553a707d9bd7190ae2839e3
Santiago Mauhourat <smauhourat@aadim.org.ar>
2022-11-02 12:08:27 -0300

    infinite first

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,5 +1,6 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { Person } from "./Person";
+import { useInfiniteQuery } from 'react-query';
 
 const initialUrl = "https://swapi.dev/api/people/";
 const fetchUrl = async (url) => {
@@ -8,6 +9,18 @@ const fetchUrl = async (url) => {
 };
 
 export function InfinitePeople() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery(
+    "sw-people",
+    ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    {
+      getNextPageParam: (lastPage) => lastPage.next || undefined,
+    }
+  );
+  return <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+      {data?.pages.map(pageData => {
+        return pageData.results.map(person => {
+          return  (<Person key={person.name} name={person.name}/>)
+        })
+      })}
+    </InfiniteScroll>;
 }
