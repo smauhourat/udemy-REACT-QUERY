bbe7cf385b32f1591278116504b8a784bf5502ed
Santiago Mauhourat <smauhourat@aadim.org.ar>
2022-11-02 20:35:04 -0300

    fix

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -12,7 +12,14 @@ async function getTreatments(): Promise<Treatment[]> {
 }
 
 export function useTreatments(): Treatment[] {
+  const toast = useCustomToast();
+
   const fallback = [];
-  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments, {
+    onError: (error) => {
+      const title =
+        error instanceof Error ? error.message : 'error connecting to server';
+    },
+  });
   return data;
 }
